3b9f8489c9b0ac2ba5aea1d03a7cb8cec73ee2e6
John Beech <johnbeech.github@mkv25.net>
2019-06-08 10:27:09 +0100

    Manage refresh tokens between requests

--- views/playlist.php
+++ views/playlist.php
@@ -2,6 +2,8 @@
 require('../vendor/autoload.php');
 require('./secrets.config.php');
 
+session_start();
+
 $session = new SpotifyWebAPI\Session(
   $SPOTIFY_CLIENT_ID,
   $SPOTIFY_CLIENT_SECRET,
@@ -10,18 +12,51 @@ $session = new SpotifyWebAPI\Session(
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 
+function output($data) {
+  header('Content-type: application/json');
+  echo json_encode($data);
+}
+
 if (isset($_GET['code'])) {
-    $session->requestAccessToken($_GET['code']);
-    $api->setAccessToken($session->getAccessToken());
+  $session->requestAccessToken($_GET['code']);
+  $_SESSION['accessToken'] = $session->getAccessToken();
+}
 
-    print_r($api->me());
+if (isset($_SESSION['refreshToken'])) {
+  $session->setRefreshToken($_SESSION['refreshToken']);
+}
+
+if(isset($_SESSION['accessToken'])) {
+  try {
+    $api->setAccessToken($_SESSION['accessToken']);
+
+    $searchTerm = $_GET['search'];
+    if (!$searchTerm) {
+      $searchTerm = 'sophie needs a ladder';
+    }
+
+    output($api->search($searchTerm, 'track'));
+
+    if ($session->refreshAccessToken($session->getRefreshToken())) {
+      $_SESSION['accessToken'] = $session->getAccessToken();
+      $_SESSION['refreshToken'] = $session->getRefreshToken();
+    }
+  }
+  catch (Exception $ex) {
+    output(array(
+      'message' => 'Not authorized',
+      'detail' => $ex->getMessage(),
+      'error' => true
+    ));
+    session_destroy();
+  }
 } else {
-    $options = [
-        'scope' => [
-            'user-read-email',
-        ],
-    ];
+  $options = [
+    'scope' => [
+      'user-read-email',
+    ]
+  ];
 
-    header('Location: ' . $session->getAuthorizeUrl($options));
-    die();
+  header('Location: ' . $session->getAuthorizeUrl($options));
+  die();
 }
--- views/secrets.config.template.php
+++ views/secrets.config.template.php
@@ -2,4 +2,4 @@
 
 $SPOTIFY_CLIENT_ID = '32charalphanumericstring';
 $SPOTIFY_CLIENT_SECRET = '32charalphanumericstring';
-$SPOTIFY_CLIENT_URL = 'protocol://the.hosted.redirect.url/playlist.php'
+$SPOTIFY_CLIENT_URL = 'protocol://the.hosted.redirect.url/playlist.php'; 
