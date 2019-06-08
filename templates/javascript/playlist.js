3c0f3fe7a7519e9a98811d33fb77d515a99d3aec
John Beech <johnbeech.github@mkv25.net>
2019-06-08 11:54:46 +0100

    Start building form for track search

--- templates/javascript/playlist.js
+++ templates/javascript/playlist.js
@@ -1,6 +1,35 @@
 var app = new Vue({
   el: '#playlist',
   data: {
-    message: 'Hello Spotify!'
+    message: 'Hello Spotify!',
+    auth: false,
+    form: {
+      searchTerm: ''
+    },
+    search: {
+      results: []
+    },
+    playlist: {
+      tracks: []
+    }
+  },
+  filters: {
+    pretty: (data) => JSON.stringify(data, null, 2)
   }
 })
+
+function getCredentials() {
+  $.getJSON('/views/playlist.php?me=1')
+    .done(data => {
+      app.auth = data
+    }).fail(( jqxhr, textStatus, error ) => {
+      app.auth = {
+        error: [textStatus, ', ', error].join('')
+      }
+    })
+}
+
+getCredentials()
+
+const playlistEl = document.getElementById('playlist')
+playlistEl.style.display = 'block';
